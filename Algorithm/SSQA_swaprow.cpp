117f791544c94f437b37c715010f8b6e8456536a
okahaya <okahaya0901@gmail.com>
2024-10-13 15:03:44 +0900

    TSPできるようになった！！！！！！！

--- Algorithm/SSQA_swaprow.cpp
+++ Algorithm/SSQA_swaprow.cpp
@@ -14,70 +14,114 @@
 
 using namespace std;
 
-std::ofstream log_file("annealing_log.csv");
-std::ofstream log_file2("montecarlo_log.csv");
+std::ofstream mc_log_file("montecarlo_log.csv");
 
-void flip_bits(vector<int>& bits, vector<int> idx1, vector<int> idx2, vector<int>& fliped_bits) {
-    fliped_bits.clear();
+void select_bits(vector<int>& bits, vector<int> idx1, vector<int> idx2, vector<int>& flip_bits) {
+    flip_bits.clear();
     for (int i=0; i<idx1.size(); ++i) {
         if (bits[idx1[i]] != bits[idx2[i]]) {
-            fliped_bits.push_back(idx1[i]);
-            fliped_bits.push_back(idx2[i]);
-            bits[idx1[i]] = 1 - bits[idx1[i]]; 
-            bits[idx2[i]] = 1 - bits[idx2[i]]; 
+            flip_bits.push_back(idx1[i]);
+            flip_bits.push_back(idx2[i]);
         }
     }
 }
 
-void monte_carlo_step(vector<vector<int>>& bits, const vector<vector<double>>& Q, double T, double Gamma, const vector<pair<vector<int>, int>>& nhot_memo, double max_dE = 1e100) {
+void flip_bits(vector<int>& bits, const vector<int>& flip_bits) {
+    for (int i=0; i<flip_bits.size(); ++i) {
+        bits[flip_bits[i]] = 1 - bits[flip_bits[i]];
+    }
+}
+
+bool Is_contains(const vector<int>& a, int N) {
+    unordered_set<int> set(a.begin(), a.end());
+    return set.find(N) != set.end();
+}
+
+double calculate_delta_E_rowswap(const vector<vector<int>> &bits, const vector<vector<double>>& Q, int layer, const vector<int>& fliped_bits, double At, double Bt) {
+    double delta_E = 0.0;
     int N = bits[0].size();
+    int L = bits.size();
+    const vector<int>& bits_layer = bits[layer];
+    vector<double> delta_bits;
+    for (int i = 0; i < fliped_bits.size(); ++i) {
+        delta_bits.push_back(static_cast<double>(-2*bits_layer[fliped_bits[i]] + 1));
+    }   
+
+    for (int i = 0; i < fliped_bits.size(); i++)
+    {
+        for (int j = 0; j < fliped_bits.size(); j++)
+        {
+            if (fliped_bits[i] <= fliped_bits[j])
+            {
+                delta_E += At*delta_bits[i]*Q[fliped_bits[i]][fliped_bits[j]]/2.0;
+            }
+            else
+            {
+                delta_E += At*delta_bits[i]*Q[fliped_bits[j]][fliped_bits[i]]/2.0;
+            }
+        }
+    }
+    for (int i = 0; i < fliped_bits.size(); i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (bits_layer[j] == 1 && (Q[fliped_bits[i]][j] != 0.0 || Q[j][fliped_bits[i]] != 0.0))
+            {
+                if (Is_contains(fliped_bits,j) == false)   
+                {
+                    delta_E += At*delta_bits[i]*(Q[fliped_bits[i]][j] + Q[j][fliped_bits[i]]);                  
+                }
+            }
+            
+        }
+        
+    }
+    
+    int next_layer = (layer + 1) % L;
+    int prev_layer = (layer - 1 + L) % L;
+    for (int i = 0; i < fliped_bits.size(); ++i) {
+        delta_E += (Bt / L) * delta_bits[i] * (bits[next_layer][fliped_bits[i]] + bits[prev_layer][fliped_bits[i]]);
+    }
+    return delta_E;
+}
+
+void monte_carlo_step(vector<vector<int>>& bits, const vector<vector<double>>& Q, int layer, double T, double Gamma, const vector<pair<vector<int>, int>>& nhot_memo, double max_dE = 1e100) {
+    int N = bits[layer].size();
     int L = bits.size();
     // double At = 1 / (L * T);
     // double Bt = (-1.0 / 2.0 * log(tanh(Gamma / (L * T))));
-    double Bt = (-1.0 / 2.0 * log(tanh(Gamma / (L * T))))*(L * T);
-    if (Bt < 1e-5)Bt = 0;
+    double Bt = 10.0*(-1.0 / 2.0 * log(tanh(Gamma / (L * T))))*(L * T);
+    if (Bt < 1e-3)Bt = 0;
     double At = 1.0;
 
     thread_local mt19937 rng(random_device{}());
     uniform_real_distribution<double> dist_real(0.0, 1.0);
-    uniform_int_distribution<int> dist_layer(0, L - 1);
     uniform_int_distribution<int> dist_nhot(0, nhot_memo.size() - 1);
 
-    #pragma omp for 
-    for (int i = 0; i < L; ++i) {
-        // int layer = dist_layer(rng);
-        int layer = i;
-        int idx1 = dist_nhot(rng);
-        int idx2 = dist_nhot(rng);
-        while (idx1 % 2 != idx2 % 2) {
-            idx2 = dist_nhot(rng);
-        }
-        double Epre = qubo_energy(bits[layer],Q);
-        vector<int> fliped_bits(1,0);
-        flip_bits(bits[layer], nhot_memo[idx1].first, nhot_memo[idx2].first, fliped_bits);
+    int idx1 = dist_nhot(rng);
+    int idx2 = dist_nhot(rng);
+    while (idx1 % 2 != idx2 % 2) {
+        idx2 = dist_nhot(rng);
+    }
 
-        double delta_E = 0;
-        delta_E += qubo_energy(bits[layer],Q)-Epre;
-        delta_E += calculate_delta_E_rowswap(bits, Q, layer, fliped_bits, 0, Bt);
-        // delta_E += calculate_delta_E_rowswap(bits, Q, layer, fliped_bits, At, Bt);
-        
-        delta_E = max(-max_dE, min(delta_E, max_dE));
+    vector<int> fliped_bits(1,0);
+    select_bits(bits[layer], nhot_memo[idx1].first, nhot_memo[idx2].first, fliped_bits);
 
-        double acceptance_probability = exp(-delta_E / T);
+    double delta_E = calculate_delta_E_rowswap(bits, Q, layer, fliped_bits, At, Bt);
     
-        bool accept = true;
-        if (dist_real(rng) >= exp(-delta_E / T) || delta_E == 0) {
-            for(int j = 0; j < fliped_bits.size(); ++j) {
-                bits[layer][fliped_bits[j]] = 1 - bits[layer][fliped_bits[j]];
-            }
-            accept = false;
-        }
-        if (layer == 0) {
-        log_file2 << "Layer: " << layer << ", Bt :" << Bt << ", At: " << At <<", Delta_E: " << delta_E << ", Acceptance Probability: " << acceptance_probability <<", Is Accepted:" << accept << "\n";
-        }
+    delta_E = max(-max_dE, min(delta_E, max_dE));
+
+    double acceptance_probability = exp(-delta_E / T);
+    
+    bool accept = false;
+    if (dist_real(rng) < exp(-delta_E / T) && delta_E != 0) {
+        flip_bits(bits[layer],fliped_bits);
+        accept = true;
+    }
+    if (layer == 0) {
+    mc_log_file << "Layer: " << layer << ", Bt :" << Bt << ", At: " << At <<", Delta_E: " << delta_E << ", Acceptance Probability: " << acceptance_probability <<", Is Accepted:" << accept << "\n";
     }
 }
-
 
 void execute_annealing(vector<vector<int>>& bits, const vector<vector<double>>& Q, int L, int N, double T, double Gamma, int anneal_steps, int mc_steps, double& duration, const vector<pair<vector<int>, int>>& nhot_memo, bool bit_initialized) {
     if (!bit_initialized) {
@@ -91,32 +135,23 @@ void execute_annealing(vector<vector<int>>& bits, const vector<vector<double>>& 
     vector<vector<double>> energies(anneal_steps, vector<double>(L, 0));
     vector<vector<double>> driver_energies(anneal_steps, vector<double>(L, 0));
 
-    log_file << "Step,Temperature,Energy\n";
-    log_file2 << "Acceptance Probability\n";
+    mc_log_file << "Acceptance Probability\n";
     showProgressBar(0, anneal_steps, "annealing step");
-    omp_set_num_threads(1);
+    omp_set_num_threads(4);
 
-    #pragma omp parallel
-    {
-        for (int i = 0; i < anneal_steps; ++i) {
-            for (int j = 0; j < mc_steps; ++j) {
-                monte_carlo_step(bits, Q, T, Gamma, nhot_memo);
+    #pragma omp parallel for
+    for (int  layer = 0; layer < L; layer++) {
+        for (int i = 0; i < anneal_steps; i++) {
+            for (int j = 0; j < mc_steps; j++) {
+                monte_carlo_step(bits,Q,layer,T,Gamma,nhot_memo);
             }
-            for (int k = 0; k < L; ++k) {
-                energies[i][k] = qubo_energy(bits[k], Q);
-                driver_energies[i][k] = driver_energy(bits, k);
-            }
+            energies[i][layer] = qubo_energy(bits[layer], Q);
+            driver_energies[i][layer] = driver_energy(bits, layer);
 
-            int tid = omp_get_thread_num();
-            if (tid == 0) {
-                double avg_energy = accumulate(energies[i].begin(), energies[i].end(), 0.0) / L;
-                double avg_driver_energy = accumulate(driver_energies[i].begin(), driver_energies[i].end(), 0.0) / L;
-                log_file << i << "," << T << "," << avg_energy << "\n";
-
+            if (layer == 0) {
                 showProgressBar(i + 1, anneal_steps, "annealing step");
                 T *= coolingrate;
-                Gamma *= gamma;
-                
+                Gamma *= gamma;        
             }
         }
     }
